8da2b78fb591b8027f710881af35060f37b38148
Wojciech Rozwałka <wojciechrozwalka@gmail.com>
2020-12-21 22:42:19 +0100

    add FakeBin, todo test

---
+++ src/test/java/javer/codewars/eightkyu/FakeBinaryTest.java
@@ -0,0 +1,14 @@
+package javer.codewars.eightkyu;
+
+import org.junit.Test;
+
+public class FakeBinaryTest {
+
+
+    // TODO: Replace examples and use TDD development by writing your own tests
+
+    @Test
+    public void testFakeBin() {
+        // assertEquals("expected", "actual");
+    }
+}
