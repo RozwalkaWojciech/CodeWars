234285b3d3e0e147c09bce2783f8ae4ff362f0f2
Wojciech Rozwałka <wojciechrozwalka@gmail.com>
2020-12-21 22:52:12 +0100

    add You Can't Code Under Pressure #1 solution with test

---
+++ src/main/java/javer/codewars/eightkyu/YouCantCodeUnderPressure1.java
@@ -0,0 +1,8 @@
+package javer.codewars.eightkyu;
+
+public class YouCantCodeUnderPressure1 {
+
+    public static int doubleInteger(int i) {
+        return i * 2;
+    }
+}
---
+++ src/test/java/javer/codewars/eightkyu/YouCantCodeUnderPressure1Test.java
@@ -0,0 +1,12 @@
+package javer.codewars.eightkyu;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class YouCantCodeUnderPressure1Test {
+
+    @Test
+    public final void testDoubleInteger() {
+        Assert.assertEquals("Nope!", 4, YouCantCodeUnderPressure1.doubleInteger(2));
+    }
+}
