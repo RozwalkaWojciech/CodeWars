64fe3c6527f1a2e339e85b272314728feddec11c
Wojciech Rozwałka <wojciechrozwalka@gmail.com>
2020-12-22 11:23:12 +0100

    clean code IsItEvenTest

--- src/test/java/javer/codewars/eightkyu/IsItEvenTest.java
+++ src/test/java/javer/codewars/eightkyu/IsItEvenTest.java
@@ -4,28 +4,29 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class IsItEvenTest {
 
     private IsItEven num;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         num = new IsItEven();
     }
 
     @After
-    public void setDown() throws Exception {
+    public void setDown() {
         num = null;
     }
 
     @Test
     public void testIsEven() {
-        assertEquals(true, num.isEven(0));
-        assertEquals(false, num.isEven(0.5));
-        assertEquals(false, num.isEven(1));
-        assertEquals(true, num.isEven(2));
-        assertEquals(true, num.isEven(-4));
+        assertTrue(num.isEven(0));
+        assertFalse(num.isEven(0.5));
+        assertFalse(num.isEven(1));
+        assertTrue(num.isEven(2));
+        assertTrue(num.isEven(-4));
     }
 }
