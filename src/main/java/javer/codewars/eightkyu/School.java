187fc4828f35fe3f3d934803f7e259fa22cc88a6
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2021-02-06 22:08:30 +0100

    School

---
+++ src/main/java/javer/codewars/eightkyu/School.java
@@ -0,0 +1,20 @@
+package javer.codewars.eightkyu;
+
+/*
+It's the academic year's end, fateful moment of your school report.
+The averages must be calculated. All the students come to you and entreat you to calculate their average for them.
+Easy ! You just need to write a script.
+Return the average of the given array rounded down to its nearest integer.
+The array will never be empty.
+*/
+
+public class School {
+
+    public static int getAverage(int[] marks) {
+        int sum = 0;
+        for (int mark : marks) {
+            sum += mark;
+        }
+        return sum / marks.length;
+    }
+}
