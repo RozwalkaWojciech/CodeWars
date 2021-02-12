c335c840fadb0e58aa72089b81dd0ac0a84e2739
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2021-02-12 22:40:26 +0100

    PlayingWithPassphrases another nice Solutions

--- src/main/java/javer/codewars/sixkyu/PlayingWithPassphrases.java
+++ src/main/java/javer/codewars/sixkyu/PlayingWithPassphrases.java
@@ -57,3 +57,27 @@ public class PlayingWithPassphrases {
     }
 }
 
+/*import java.util.stream.Collectors;
+
+public class PlayPass {
+    private static int count = 0;
+
+    public static String playPass(final String s, final int n) {
+        count = 0;
+
+        return new StringBuilder()
+                .append(
+                        s.toUpperCase()
+                                .chars()
+                                .map(i -> Character.isDigit(i) ? 57 - Character.getNumericValue(i) : i)
+                                .mapToObj(c -> (char) c)
+                                .map(c -> (c <= 90 && c >= 65) ? (char) (c + (n % 26)) : c)
+                                .map(c -> c > 90 ? (char) ((c % 91) + 65) : c)
+                                .map(i -> i.toString())
+                                .map(i -> (count++ % 2) == 0 ? i.toUpperCase() : i.toLowerCase())
+                                .collect(Collectors.joining("")))
+                .reverse()
+                .toString();
+    }
+}*/
+
