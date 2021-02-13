35ecb34f7befc4ca013fd9590490fc640a40bffd
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2021-02-13 14:39:15 +0100

    WhoLikesIt another solution

--- src/main/java/javer/codewars/sixkyu/WhoLikesIt.java
+++ src/main/java/javer/codewars/sixkyu/WhoLikesIt.java
@@ -26,4 +26,19 @@ public class WhoLikesIt {
         }
         return "no one likes this";
     }
+
+    public static String whoLikesIt2(String... nms) {
+        switch (nms.length) {
+            case 0:
+                return "no one likes this";
+            case 1:
+                return String.format("%s likes this", nms[0]);
+            case 2:
+                return String.format("%s and %s like this", nms[0], nms[1]);
+            case 3:
+                return String.format("%s, %s and %s like this", nms[0], nms[1], nms[2]);
+            default:
+                return String.format("%s, %s and %d others like this", nms[0], nms[1], nms.length - 2);
+        }
+    }
 }
