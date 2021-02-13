6b027abb44da0ad47aa3845d13c0a1261cd796dc
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2021-02-13 13:19:04 +0100

    MultiplesOf3or5 solution

--- src/main/java/javer/codewars/sixkyu/MultiplesOf3or5.java
+++ src/main/java/javer/codewars/sixkyu/MultiplesOf3or5.java
@@ -3,7 +3,17 @@ package javer.codewars.sixkyu;
 public class MultiplesOf3or5 {
     public int solution(int number) {
 
+        if (number < 0) {
+            return -1;
+        }
 
-        return 0;
+        int result = 0;
+
+        for (int i = 3; i < number; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                result += i;
+            }
+        }
+        return result;
     }
 }
