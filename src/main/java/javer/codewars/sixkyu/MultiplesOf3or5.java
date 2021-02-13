bc6ded52c9a790c9a65c7a0b8cbca63f434e0717
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2021-02-13 13:19:31 +0100

    MultiplesOf3or5 best solution

--- src/main/java/javer/codewars/sixkyu/MultiplesOf3or5.java
+++ src/main/java/javer/codewars/sixkyu/MultiplesOf3or5.java
@@ -1,5 +1,7 @@
 package javer.codewars.sixkyu;
 
+import java.util.stream.IntStream;
+
 public class MultiplesOf3or5 {
     public int solution(int number) {
 
@@ -15,5 +17,12 @@ public class MultiplesOf3or5 {
             }
         }
         return result;
+    }
+
+    //best way
+    public int solution2(int number) {
+        return IntStream.range(0, number)
+                .filter(n -> (n % 3 == 0) || (n % 5 == 0))
+                .sum();
     }
 }
