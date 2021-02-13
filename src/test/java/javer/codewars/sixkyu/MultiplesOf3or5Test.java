d0c5d7d095a21509ae81ffda267ad74e3ea9b775
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2021-02-13 13:31:08 +0100

    MultiplesOf3or5Test add new test

--- src/test/java/javer/codewars/sixkyu/MultiplesOf3or5Test.java
+++ src/test/java/javer/codewars/sixkyu/MultiplesOf3or5Test.java
@@ -5,8 +5,29 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class MultiplesOf3or5Test {
+
     @Test
     public void test() {
         assertEquals(23, new MultiplesOf3or5().solution(10));
+    }
+
+    @Test
+    public void shouldReturnCorrectSum(){
+        //given
+        int number = 15;
+        //when
+        int sum = new MultiplesOf3or5().solution2(number);
+        //then
+        assertEquals(45, sum);
+    }
+
+    @Test
+    public void shouldReturnZeroWhenIsNegative(){
+        //given
+        int number = -2;
+        //when
+        int sum = new MultiplesOf3or5().solution2(number);
+        //then
+        assertEquals(0, sum);
     }
 }
