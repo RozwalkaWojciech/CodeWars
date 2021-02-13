fb894024e01733da6b0998e1a9430948ac73ee7e
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2021-02-13 13:03:36 +0100

    MultiplesOf3or5 start and test

---
+++ src/main/java/javer/codewars/sixkyu/MultiplesOf3or5.java
@@ -0,0 +1,9 @@
+package javer.codewars.sixkyu;
+
+public class MultiplesOf3or5 {
+    public int solution(int number) {
+
+
+        return 0;
+    }
+}
---
+++ src/test/java/javer/codewars/sixkyu/MultiplesOf3or5Test.java
@@ -0,0 +1,12 @@
+package javer.codewars.sixkyu;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MultiplesOf3or5Test {
+    @Test
+    public void test() {
+        assertEquals(23, new MultiplesOf3or5().solution(10));
+    }
+}
