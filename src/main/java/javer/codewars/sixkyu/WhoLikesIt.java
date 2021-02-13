d0c905e6f9311b6ae64c30fbeb00e57c1d858378
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2021-02-13 14:38:30 +0100

    WhoLikesIt solution

--- src/main/java/javer/codewars/sixkyu/WhoLikesIt.java
+++ src/main/java/javer/codewars/sixkyu/WhoLikesIt.java
@@ -11,7 +11,19 @@ likes {"Alex", "Jacob", "Mark", "Max"} // must be "Alex, Jacob and 2 others like
 
 public class WhoLikesIt {
     public static String whoLikesIt(String... names) {
-        //Do your magic here
-        return "";
+
+        if (names.length == 1) {
+            return names[0] + " likes this";
+        }
+        if (names.length == 2) {
+            return names[0] + " and " + names[1] + " like this";
+        }
+        if (names.length == 3) {
+            return names[0] + ", " + names[1] + " and " + names[2] + " like this";
+        }
+        if (names.length > 3) {
+            return names[0] + ", " + names[1] + " and " + (names.length - 2) + " others like this";
+        }
+        return "no one likes this";
     }
 }
