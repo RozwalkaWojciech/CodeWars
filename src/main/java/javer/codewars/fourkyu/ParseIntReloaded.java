14c7a1254b5b235aa52b4f1f1ca6cb0d1ce14c38
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2021-02-15 22:07:33 +0100

    ParseIntReloaded solution near end

--- src/main/java/javer/codewars/fourkyu/ParseIntReloaded.java
+++ src/main/java/javer/codewars/fourkyu/ParseIntReloaded.java
@@ -17,11 +17,57 @@ The "and" in e.g. "one hundred and twenty-four" is optional, in some cases it's 
 All tested numbers are valid, you don't need to validate them
 */
 
+import java.util.Map;
+
 public class ParseIntReloaded {
 
     public static int parseInt(String numStr) {
-        // Your code here!
-        return -1;
+
+        int result = 0;
+
+        Map<String, Integer> words = Map.ofEntries(
+                Map.entry("zero", 0),
+                Map.entry("one", 1),
+                Map.entry("two", 2),
+                Map.entry("three", 3),
+                Map.entry("four", 4),
+                Map.entry("five", 5),
+                Map.entry("six", 6),
+                Map.entry("seven", 7),
+                Map.entry("eight", 8),
+                Map.entry("nine", 9),
+                Map.entry("ten", 10),
+                Map.entry("eleven", 11),
+                Map.entry("twelve", 12),
+                Map.entry("thirteen", 13),
+                Map.entry("fourteen", 14),
+                Map.entry("fifteen", 15),
+                Map.entry("sixteen", 16),
+                Map.entry("seventeen", 17),
+                Map.entry("eighteen", 18),
+                Map.entry("nineteen", 19),
+                Map.entry("twenty", 20),
+                Map.entry("thirty", 30),
+                Map.entry("forty", 40),
+                Map.entry("fifty", 50),
+                Map.entry("sixty", 60),
+                Map.entry("seventy", 70),
+                Map.entry("eighty", 80),
+                Map.entry("ninety", 90));
+
+        Map<String, Integer> mult = Map.of("hundred", 100, "thousand", 1000, "million", 1000000);
+
+        String[] arr = numStr.split(" |-");
+
+        for (String word : arr) {
+            if (words.containsKey(word)) {
+                result += words.get(word);
+            }
+            if (mult.containsKey(word)) {
+                result *= mult.get(word);
+            }
+        }
+        return result;
     }
 
     public static void main(String[] args) {
