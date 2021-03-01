233f07ad11bd66722def77ddb7654e72e19cf7aa
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2021-03-01 13:33:22 +0100

    DescendingOrder solutions

--- src/main/java/javer/codewars/sevenkyu/DescendingOrder.java
+++ src/main/java/javer/codewars/sevenkyu/DescendingOrder.java
@@ -1,9 +1,17 @@
 package javer.codewars.sevenkyu;
 
+import java.util.Arrays;
+
 public class DescendingOrder {
     public static int sortDesc(final int num) {
 
 
-        return 0;
+        StringBuilder sb = new StringBuilder();
+        char[] chars = String.valueOf(num).toCharArray();
+        Arrays.sort(chars);
+        for (char n : chars) {
+            sb.append(n);
+        }
+        return Integer.parseInt(sb.reverse().toString());
     }
 }
