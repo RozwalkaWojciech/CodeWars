2dd516f44527c0ff3d876c0062c398ab9bdeb7b5
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2021-03-01 13:37:37 +0100

    DescendingOrder functional solutions

--- src/main/java/javer/codewars/sevenkyu/DescendingOrder.java
+++ src/main/java/javer/codewars/sevenkyu/DescendingOrder.java
@@ -1,8 +1,11 @@
 package javer.codewars.sevenkyu;
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.Collectors;
 
 public class DescendingOrder {
+
     public static int sortDesc(final int num) {
 
 
@@ -13,5 +16,14 @@ public class DescendingOrder {
             sb.append(n);
         }
         return Integer.parseInt(sb.reverse().toString());
+    }
+
+    public static int sortDecsFunctional(final int num) {
+        return Integer.parseInt(String.valueOf(num)
+                .chars()
+                .mapToObj(value -> String.valueOf(Character.getNumericValue(value)))
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.joining())
+        );
     }
 }
