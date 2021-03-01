db0ff75f8fe594795f8125828b2b3445f668aad1
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2021-03-01 21:28:21 +0100

    DetermineTheDateByTheDayNumber start and test

---
+++ src/main/java/javer/codewars/sixkyu/DetermineTheDateByTheDayNumber.java
@@ -0,0 +1,9 @@
+package javer.codewars.sixkyu;
+
+public class DetermineTheDateByTheDayNumber {
+
+    public static String getDay(int day, boolean isLeap) {
+
+        return "";
+    }
+}
---
+++ src/test/java/javer/codewars/sixkyu/DetermineTheDateByTheDayNumberTest.java
@@ -0,0 +1,19 @@
+package javer.codewars.sixkyu;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DetermineTheDateByTheDayNumberTest {
+
+    @Test
+    void basicTest() {
+        assertEquals("January, 15", DetermineTheDateByTheDayNumber.getDay(15, false));
+        assertEquals("February, 10", DetermineTheDateByTheDayNumber.getDay(41, false));
+        assertEquals("February, 28", DetermineTheDateByTheDayNumber.getDay(59, false));
+        assertEquals("March, 1", DetermineTheDateByTheDayNumber.getDay(60, false));
+        assertEquals("February, 29", DetermineTheDateByTheDayNumber.getDay(60, true));
+        assertEquals("December, 31", DetermineTheDateByTheDayNumber.getDay(365, false));
+        assertEquals("December, 31", DetermineTheDateByTheDayNumber.getDay(366, true));
+    }
+}
