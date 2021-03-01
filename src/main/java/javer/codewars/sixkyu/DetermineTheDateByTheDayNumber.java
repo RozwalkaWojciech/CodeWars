f90b6c0a08da6f624cb02775e317b055e842c634
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2021-03-01 22:03:56 +0100

    DetermineTheDateByTheDayNumber solution

--- src/main/java/javer/codewars/sixkyu/DetermineTheDateByTheDayNumber.java
+++ src/main/java/javer/codewars/sixkyu/DetermineTheDateByTheDayNumber.java
@@ -1,9 +1,13 @@
 package javer.codewars.sixkyu;
 
+import java.time.LocalDate;
+
 public class DetermineTheDateByTheDayNumber {
 
     public static String getDay(int day, boolean isLeap) {
-
-        return "";
+        LocalDate date = isLeap ? LocalDate.ofYearDay(2020, day) : LocalDate.ofYearDay(2019, day);
+        return date.getMonth().toString().charAt(0)
+                + date.getMonth().toString().substring(1).toLowerCase()
+                + ", " + date.getDayOfMonth();
     }
 }
