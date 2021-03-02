b7c51c94fdf1d026c708a883ed2c014904b66780
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2021-03-02 11:00:23 +0100

    Scramblies solution

--- src/main/java/javer/codewars/fivekyu/Scramblies.java
+++ src/main/java/javer/codewars/fivekyu/Scramblies.java
@@ -3,6 +3,17 @@ package javer.codewars.fivekyu;
 public class Scramblies {
     public static boolean scramble(String str1, String str2) {
 
+        for (String s : str2.split("")) {
+            if (!str1.contains(s)) {
+                return false;
+            }
+            str1 = str1.replaceFirst(s, "");
+        }
         return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(scramble("rkqodlww", "world"));
+        System.out.println(scramble("rkqodl", "world"));
     }
 }
