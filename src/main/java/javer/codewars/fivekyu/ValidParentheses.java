010d4b3d457a3dc2b6716d9e9425aeeb98a7c405
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2021-03-02 12:06:54 +0100

    ValidParentheses solution

--- src/main/java/javer/codewars/fivekyu/ValidParentheses.java
+++ src/main/java/javer/codewars/fivekyu/ValidParentheses.java
@@ -1,5 +1,7 @@
 package javer.codewars.fivekyu;
 
+import java.util.ArrayDeque;
+
 /*
 Write a function that takes a string of parentheses, and determines if the order of the parentheses is valid.
 The function should return true if the string is valid, and false if it's invalid.
@@ -20,6 +22,18 @@ public class ValidParentheses {
 
     public static boolean validParentheses(String parens) {
 
-        return true;
+        var stack = new ArrayDeque<Character>();
+
+        for (char c : parens.toCharArray()) {
+            if (c == '(') {
+                stack.push(c);
+            } else if (c == ')') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
     }
 }
