61215948d6dd716f6fef74504314b1d60e4071f3
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2021-03-05 22:50:31 +0100

    NextBiggerNumberWithTheSameDigits another solution

--- src/main/java/javer/codewars/fourkyu/NextBiggerNumberWithTheSameDigits.java
+++ src/main/java/javer/codewars/fourkyu/NextBiggerNumberWithTheSameDigits.java
@@ -1,6 +1,7 @@
 package javer.codewars.fourkyu;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 
 /*
@@ -45,6 +46,22 @@ public class NextBiggerNumberWithTheSameDigits {
                 return Long.parseLong(sb.toString());
             } else {
                 rightDigits.add(Character.getNumericValue(strNum.charAt(i)));
+            }
+        }
+        return -1;
+    }
+
+    public static long nextBiggerNumber2(long n) {
+        char[] s = String.valueOf(n).toCharArray();
+        for (int i = s.length - 2; i >= 0; i--) {
+            for (int j = s.length - 1; j > i; j--) {
+                if (s[i] < s[j]) {
+                    char tmp = s[i];
+                    s[i] = s[j];
+                    s[j] = tmp;
+                    Arrays.sort(s, i + 1, s.length);
+                    return Long.parseLong(String.valueOf(s));
+                }
             }
         }
         return -1;
