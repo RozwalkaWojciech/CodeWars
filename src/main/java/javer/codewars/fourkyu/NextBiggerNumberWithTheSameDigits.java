4e69f97a4aa8062690a439048bbd642531d73512
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2021-03-05 22:49:21 +0100

    NextBiggerNumberWithTheSameDigits solution

--- src/main/java/javer/codewars/fourkyu/NextBiggerNumberWithTheSameDigits.java
+++ src/main/java/javer/codewars/fourkyu/NextBiggerNumberWithTheSameDigits.java
@@ -1,5 +1,8 @@
 package javer.codewars.fourkyu;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 /*
 Create a function that takes a positive integer and returns the next bigger number that can be formed by rearranging its digits.
 For example:
@@ -14,25 +17,34 @@ If the digits can't be rearranged to form a bigger number, return -1 (or nil in 
 public class NextBiggerNumberWithTheSameDigits {
     public static long nextBiggerNumber(long n) {
 
-        String strNum = String.valueOf(n);
-        var sb = new StringBuilder(strNum);
+        var strNum = String.valueOf(n);
+        var sb = new StringBuilder();
+        var rightDigits = new ArrayList<Integer>();
 
         for (int i = strNum.length() - 1; i > 0; i--) {
-            if (strNum.charAt(i) == '0' && strNum.charAt(i - 1) != '0') {
-                if ((i - 2) >= (strNum.length() - i) && strNum.charAt(i - 2) < strNum.charAt(i - 1)) {
-                    sb.setCharAt(i - 2, strNum.charAt(i));
-                    sb.setCharAt(i, strNum.charAt(i - 2));
-                    sb.setCharAt(i-1, '0');
-                } else {
-                    sb.setCharAt(i - 1, strNum.charAt(i));
-                    sb.setCharAt(i, strNum.charAt(i - 1));
-                    i--;
-                }
-            } else if (strNum.charAt(i) > strNum.charAt(i - 1)) {
-                sb.setCharAt(i - 1, strNum.charAt(i));
-                sb.setCharAt(i, strNum.charAt(i - 1));
 
+            if (strNum.charAt(i) > strNum.charAt(i - 1)) {
+
+                sb.append(strNum, 0, i - 1);
+
+                rightDigits.add(Character.getNumericValue(strNum.charAt(i)));
+                rightDigits.add(Character.getNumericValue(strNum.charAt(i - 1)));
+
+                Collections.sort(rightDigits);
+
+                for (int j = 0; j < rightDigits.size(); j++) {
+                    if (rightDigits.get(j) > Character.getNumericValue(strNum.charAt(i - 1))) {
+                        sb.append(rightDigits.get(j));
+                        rightDigits.remove(rightDigits.get(j));
+                        break;
+                    }
+                }
+                for (int digit : rightDigits) {
+                    sb.append(digit);
+                }
                 return Long.parseLong(sb.toString());
+            } else {
+                rightDigits.add(Character.getNumericValue(strNum.charAt(i)));
             }
         }
         return -1;
@@ -45,5 +57,9 @@ public class NextBiggerNumberWithTheSameDigits {
         System.out.println(nextBiggerNumber(154983573));
         //expected:<1234567908> but was:<1234568709>
         System.out.println(nextBiggerNumber(1234567890));
+        //expected:<1640951015> but was:<1640955011>
+        System.out.println(nextBiggerNumber(1640950511));
+        //expected:<598848484 8 3559> but was:<598848484 9 3558>
+        System.out.println(nextBiggerNumber(59884848459853L));
     }
 }
