eb30c9633ccf4e1173d7ddb68739d0e23060d0de
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2021-03-17 10:51:49 +0100

    AnEnglishTwistOnJapaneseClassic solution3

--- src/main/java/javer/codewars/sevenkyu/AnEnglishTwistOnJapaneseClassic.java
+++ src/main/java/javer/codewars/sevenkyu/AnEnglishTwistOnJapaneseClassic.java
@@ -2,6 +2,7 @@ package javer.codewars.sevenkyu;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class AnEnglishTwistOnJapaneseClassic {
 
@@ -31,7 +32,23 @@ public class AnEnglishTwistOnJapaneseClassic {
     public static List<String> theGame2(List<String> words) {
         final List<String> list = new ArrayList<>();
         String prev = words.isEmpty() || words.get(0).isEmpty() ? "" : "" + words.get(0).charAt(0);
-        for (String w : words) if (!w.isEmpty() && prev.endsWith(""+w.charAt(0))) list.add(prev = w); else break;
+        for (String w : words)
+            if (!w.isEmpty() && prev.endsWith("" + w.charAt(0))) list.add(prev = w);
+            else break;
         return list;
     }
+
+    public static List<String> theGame3(List<String> words) {
+
+        if (words.isEmpty() || words.get(0).isEmpty())
+            return new ArrayList<String>();
+
+        int x = IntStream.range(1, words.size())
+                .filter(i -> words.get(i).isEmpty()
+                        || words.get(i - 1).charAt(words.get(i - 1).length() - 1) != words.get(i).charAt(0))
+                .findFirst()
+                .orElse(words.size());
+        return words.subList(0, x);
+    }
+
 }
