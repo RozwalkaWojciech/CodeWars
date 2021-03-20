cdb81c7b3bb361d1bfcd5eae2b4887f232ceb749
Wojciech <wojciechrozwalka@gmail.com>
2021-03-20 22:31:20 +0100

    SumMixedArray init and test

---
+++ src/main/java/javer/codewars/eightkyu/SumMixedArray.java
@@ -0,0 +1,13 @@
+package javer.codewars.eightkyu;
+
+import java.util.List;
+
+public class SumMixedArray {
+
+    /*
+     * Assume input will be only of Integer o String type
+     */
+    public int sum(List<?> mixed) {
+        return -1;
+    }
+}
