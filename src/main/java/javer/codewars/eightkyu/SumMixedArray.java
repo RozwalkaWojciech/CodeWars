c1c94ab59c720d09f5f91b6a78595f616de05be1
Wojciech <wojciechrozwalka@gmail.com>
2021-03-21 18:22:45 +0100

    SumMixedArray solution

--- src/main/java/javer/codewars/eightkyu/SumMixedArray.java
+++ src/main/java/javer/codewars/eightkyu/SumMixedArray.java
@@ -8,11 +8,10 @@ Return your answer as a number.
 import java.util.List;
 
 public class SumMixedArray {
-
     /*
      * Assume input will be only of Integer o String type
      */
     public int sum(List<?> mixed) {
-        return -1;
+        return mixed.stream().mapToInt(o -> Integer.parseInt(o.toString())).sum();
     }
 }
