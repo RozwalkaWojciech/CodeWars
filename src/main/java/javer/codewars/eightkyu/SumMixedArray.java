63dbb2786addc12f7abb7c2d4310f4b8b9eaf9b4
Wojciech <wojciechrozwalka@gmail.com>
2021-03-21 18:11:28 +0100

    SumMixedArray instructions

--- src/main/java/javer/codewars/eightkyu/SumMixedArray.java
+++ src/main/java/javer/codewars/eightkyu/SumMixedArray.java
@@ -1,5 +1,10 @@
 package javer.codewars.eightkyu;
 
+/*
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+Return your answer as a number.
+ */
+
 import java.util.List;
 
 public class SumMixedArray {
