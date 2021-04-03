5129a5ac0a413ae760e0607de0c20ef7fb25c940
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2021-04-03 20:28:24 +0200

    RemoveDuplicateWords instructions and start

---
+++ src/main/java/javer/codewars/sevenkyu/RemoveDuplicateWords.java
@@ -0,0 +1,19 @@
+package javer.codewars.sevenkyu;
+
+/*
+Your task is to remove all duplicate words from a string, leaving only single (first) words entries.
+
+Input:
+'alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'
+
+Output:
+'alpha beta gamma delta'
+ */
+
+public class RemoveDuplicateWords {
+
+    public static String removeDuplicateWords(String s) {
+        // Your code here, good sir.
+        return null;
+    }
+}
