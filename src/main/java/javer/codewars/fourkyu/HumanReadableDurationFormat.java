128474375653155448140618d2d0f184e4143eea
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2021-05-02 20:29:30 +0200

    HumanReadableDurationFormat solution

--- src/main/java/javer/codewars/fourkyu/HumanReadableDurationFormat.java
+++ src/main/java/javer/codewars/fourkyu/HumanReadableDurationFormat.java
@@ -23,9 +23,62 @@ A unit of time must be used "as much as possible". It means that the function sh
  Formally, the duration specified by of a component must not be greater than any valid more significant unit of time.
  */
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class HumanReadableDurationFormat {
 
     public static String formatDuration(int seconds) {
-        return "";
+        if (seconds == 0) {
+            return "now";
+        }
+
+        int oneMinute = 60;
+        int oneHour = 60 * oneMinute;
+        int oneDay = 24 * oneHour;
+        int oneYear = 365 * oneDay;
+
+        Queue<String> queue = new LinkedList<>();
+
+        int year = seconds / oneYear;
+        if (year == 1) queue.add(year + " year");
+        else if (year > 1) queue.add(year + " years");
+        seconds = seconds - year * oneYear;
+
+        int day = seconds / oneDay;
+        if (day == 1) queue.add(day + " day");
+        else if (day > 1) queue.add(day + " days");
+        seconds = seconds - day * oneDay;
+
+        int hour = seconds / oneHour;
+        if (hour == 1) queue.add(hour + " hour");
+        else if (hour > 1) queue.add(hour + " hours");
+        seconds = seconds - hour * oneHour;
+
+        int minute = seconds / oneMinute;
+        if (minute == 1) queue.add(minute + " minute");
+        else if (minute > 1) queue.add(minute + " minutes");
+        seconds = seconds - minute * oneMinute;
+
+        if (seconds == 1) queue.add(seconds + " second");
+        else if (seconds > 1) queue.add(seconds + " seconds");
+
+        var sb = new StringBuilder();
+
+        while (!queue.isEmpty()) {
+            String tmp = queue.poll();
+            sb.append(tmp);
+            if (queue.size() > 1) {
+                sb.append(", ");
+            } else if (queue.size() == 1) {
+                sb.append(" and ");
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(formatDuration(3606));
     }
 }
+
