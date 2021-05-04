ac22d03d26c826c3624efdbe1a6366732c62e286
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2021-05-04 14:36:11 +0200

    HumanReadableDurationFormat solution3

--- src/main/java/javer/codewars/fourkyu/HumanReadableDurationFormat.java
+++ src/main/java/javer/codewars/fourkyu/HumanReadableDurationFormat.java
@@ -97,6 +97,24 @@ public class HumanReadableDurationFormat {
         return time == 0 ? "" : time + " " + s + (time == 1 ? "" : "s");
     }
 
+    public static String formatDuration3(int seconds) {
+        String res = "";
+        int[] units = new int[]{31536000, 86400, 3600, 60, 1};
+        String[] labels = new String[]{"year", "day", "hour", "minute", "second"};
+
+        if (seconds == 0) return "now";
+
+        for (int i = 0; i < 5; i++) {
+            if (seconds >= units[i]) {
+                int q = seconds / units[i];
+                seconds = seconds % units[i];
+                res += (res.equals("") ? "" : (seconds == 0 ? " and " : ", "))
+                        + q + " " + labels[i] + (q > 1 ? "s" : "");
+            }
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         System.out.println(formatDuration(3606));
     }
