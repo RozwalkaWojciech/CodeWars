71d28f92f512fd36fae922f3522589a4607d9cb9
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2021-05-17 21:17:57 +0200

    LargestPairSumInArray start and instruction

---
+++ src/main/java/javer/codewars/sevenkyu/LargestPairSumInArray.java
@@ -0,0 +1,16 @@
+package javer.codewars.sevenkyu;
+
+/*
+Given a sequence of numbers, find the largest pair sum in the sequence.
+
+For example
+[10, 14, 2, 23, 19] -->  42 (= 23 + 19)
+[99, 2, 2, 23, 19]  --> 122 (= 99 + 23)
+Input sequence contains minimum two elements and every element is an integer.
+ */
+
+public class LargestPairSumInArray {
+    public static int largestPairSum(int[] numbers) {
+        return 0;
+    }
+}
