73b5482564d98fd50c1297e57a5b994163ef4fc3
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2021-05-19 10:14:34 +0200

    LargestPairSumInArray solution

--- src/main/java/javer/codewars/sevenkyu/LargestPairSumInArray.java
+++ src/main/java/javer/codewars/sevenkyu/LargestPairSumInArray.java
@@ -9,8 +9,22 @@ For example
 Input sequence contains minimum two elements and every element is an integer.
  */
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class LargestPairSumInArray {
     public static int largestPairSum(int[] numbers) {
-        return 0;
+        return Arrays.stream(numbers)
+                .boxed()
+                .sorted()
+                .collect(Collectors.toList())
+                .subList(numbers.length - 2, numbers.length)
+                .stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(largestPairSum(new int[]{10, 14, 2, 23, 19}));
     }
 }
