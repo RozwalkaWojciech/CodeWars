0d84038197a497598eb17c08f250f2e8c2f76d0f
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2021-05-21 08:11:06 +0200

    LargestPairSumInArray solution3 simple

--- src/main/java/javer/codewars/sevenkyu/LargestPairSumInArray.java
+++ src/main/java/javer/codewars/sevenkyu/LargestPairSumInArray.java
@@ -35,6 +35,11 @@ public class LargestPairSumInArray {
                 .sum();
     }
 
+    public static int largestPairSum3(int[] numbers) {
+        Arrays.sort(numbers);
+        return numbers[numbers.length - 1] + numbers[numbers.length - 2];
+    }
+
     public static void main(String[] args) {
         System.out.println(largestPairSum(new int[]{10, 14, 2, 23, 19}));
     }
