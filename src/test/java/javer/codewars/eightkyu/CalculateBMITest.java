fc591b2338c2063accf15b77df8ae0c8cf2ae563
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2021-05-28 22:40:13 +0200

    CalculateBMI add tests

--- src/test/java/javer/codewars/eightkyu/CalculateBMITest.java
+++ src/test/java/javer/codewars/eightkyu/CalculateBMITest.java
@@ -8,5 +8,7 @@ public class CalculateBMITest {
     @Test
     public void testBMI() {
         assertEquals("Normal", CalculateBMI.bmi(80, 1.80));
+        assertEquals("Underweight", CalculateBMI.bmi(20, 1.80));
+        assertEquals("Obese", CalculateBMI.bmi(120, 1.80));
     }
 }
