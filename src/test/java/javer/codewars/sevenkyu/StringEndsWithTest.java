c9fe22ed9d275f7c291f29089147ae34b877cf1a
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2021-05-28 22:10:43 +0200

    StringEndsWith test

---
+++ src/test/java/javer/codewars/sevenkyu/StringEndsWithTest.java
@@ -0,0 +1,12 @@
+package javer.codewars.sevenkyu;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class StringEndsWithTest {
+    @Test
+    public void testSomething() {
+        assertTrue(StringEndsWith.solution("a", "bla"));
+    }
+}
