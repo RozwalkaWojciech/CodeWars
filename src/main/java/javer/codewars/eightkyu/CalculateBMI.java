b9ef47dbc9468696ae7f8a66fd4c0f72a85184d7
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2021-05-28 22:47:05 +0200

    CalculateBMI if-else-if solution

--- src/main/java/javer/codewars/eightkyu/CalculateBMI.java
+++ src/main/java/javer/codewars/eightkyu/CalculateBMI.java
@@ -11,6 +11,18 @@ if bmi > 30 return "Obese"
 
 public class CalculateBMI {
     public static String bmi(double weight, double height) {
-        return "";
+        if (bmiCalculator(weight, height) <= 18.5) {
+            return "Underweight";
+        } else if (bmiCalculator(weight, height) <= 25.0) {
+            return "Normal";
+        } else if (bmiCalculator(weight, height) <= 30.0) {
+            return "Overweight";
+        } else {
+            return "Obese";
+        }
+    }
+
+    private static double bmiCalculator(double weight, double height) {
+        return weight / Math.pow(height, 2);
     }
 }
