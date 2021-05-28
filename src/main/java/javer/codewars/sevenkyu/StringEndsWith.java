3365b8ac64a323b19f2d09ee9f0aa73000c6e0c6
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2021-05-28 22:02:31 +0200

    StringEndsWith start and instruction

---
+++ src/main/java/javer/codewars/sevenkyu/StringEndsWith.java
@@ -0,0 +1,17 @@
+package javer.codewars.sevenkyu;
+
+/*
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+Examples:
+strEndsWith('abc', 'bc') -- returns true
+strEndsWith('abc', 'd') -- returns false
+ */
+
+public class StringEndsWith {
+    public static boolean solution(String str, String ending) {
+        return false;
+    }
+}
+
+
