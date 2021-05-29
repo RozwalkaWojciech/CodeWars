b5020960ee77309737614337b8075ae5cd87ddcf
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2021-05-29 21:49:07 +0200

    GrowthOfPopulation solution

--- src/main/java/javer/codewars/sevenkyu/GrowthOfPopulation.java
+++ src/main/java/javer/codewars/sevenkyu/GrowthOfPopulation.java
@@ -28,7 +28,18 @@ Note:
 Don't forget to convert the percent parameter as a percentage in the body of your function: if the parameter percent is 2 you have to convert it to 0.02.
  */
 public class GrowthOfPopulation {
+
     public static int nbYear(int p0, double percent, int aug, int p) {
-        return 0;
+        int year = 0;
+        while (p0 < p) {
+            p0 += p0 * percent / 100 + aug;
+            year++;
+        }
+        return year;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(nbYear(1500, 5, 100, 5000));
+        System.out.println(nbYear(1500000, 2.5, 10000, 2000000));
     }
 }
