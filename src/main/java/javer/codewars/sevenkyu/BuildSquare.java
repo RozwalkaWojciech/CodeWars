70d70c8de870acdc9b630dc61dcc250ca89cef2a
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2021-05-31 14:58:14 +0200

    BuildSquare solution2 Stream

--- src/main/java/javer/codewars/sevenkyu/BuildSquare.java
+++ src/main/java/javer/codewars/sevenkyu/BuildSquare.java
@@ -11,10 +11,20 @@ n = 3, so I expect a 3x3 square back just like below as a string:
 +++
  */
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class BuildSquare {
 
     public static final String generateShape(int n) {
         return "+".repeat(n).concat("\n").repeat(n).trim();
+    }
+
+    public static final String generateShape2(int n) {
+        return IntStream
+                .range(0, n)
+                .mapToObj(value -> "+".repeat(n))
+                .collect(Collectors.joining("\n"));
     }
 
     public static void main(String[] args) {
