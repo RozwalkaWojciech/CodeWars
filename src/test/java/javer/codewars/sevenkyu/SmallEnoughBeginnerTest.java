fda911fec87756e75cf6e6450feaaa4a5f32d6ea
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2021-06-03 12:30:43 +0200

    SmallEnoughBeginner test

---
+++ src/test/java/javer/codewars/sevenkyu/SmallEnoughBeginnerTest.java
@@ -0,0 +1,15 @@
+package javer.codewars.sevenkyu;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SmallEnoughBeginnerTest {
+    @Test
+    public void basicTests() {
+        assertEquals(true, SmallEnoughBeginner.smallEnough(new int[]{66, 101}, 200));
+        assertEquals(false, SmallEnoughBeginner.smallEnough(new int[]{78, 117, 110, 99, 104, 117, 107, 115}, 100));
+        assertEquals(true, SmallEnoughBeginner.smallEnough(new int[]{101, 45, 75, 105, 99, 107}, 107));
+        assertEquals(true, SmallEnoughBeginner.smallEnough(new int[]{80, 117, 115, 104, 45, 85, 112, 115}, 120));
+    }
+}
