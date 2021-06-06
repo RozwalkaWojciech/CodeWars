55438120722c9b822fa03c7a3a3dbb3606a09e58
Wojciech <wojciechrozwalka@gmail.com>
2021-06-06 13:31:31 +0200

    RemoveFirstAndLastCharacter solution2

--- src/main/java/javer/codewars/eightkyu/RemoveFirstAndLastCharacter.java
+++ src/main/java/javer/codewars/eightkyu/RemoveFirstAndLastCharacter.java
@@ -12,4 +12,8 @@ public class RemoveFirstAndLastCharacter {
     public static String remove(String str) {
         return str.substring(1, str.length() - 1);
     }
+
+    public static String remove2(String str) {
+        return str.replaceAll("^.|.$", "");
+    }
 }
