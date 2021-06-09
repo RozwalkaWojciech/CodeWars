b5b30f94cd1363d7e67f93066b43b2d24d6e51cc
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2021-06-09 22:56:34 +0200

    StringRepeat start and instruction

---
+++ src/main/java/javer/codewars/eightkyu/StringRepeat.java
@@ -0,0 +1,15 @@
+package javer.codewars.eightkyu;
+
+/*
+Write a function called repeatStr which repeats the given string string exactly n times.
+
+repeatStr(6, "I") // "IIIIII"
+repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+ */
+
+public class StringRepeat {
+
+    public static String repeatStr(final int repeat, final String string) {
+        return "";
+    }
+}
