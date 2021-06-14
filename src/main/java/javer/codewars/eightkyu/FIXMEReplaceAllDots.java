21f40804d739f776bed7d0e28e4e24ad1eaee9be
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2021-06-14 22:00:52 +0200

    FIXMEReplaceAllDots start and instruction

---
+++ src/main/java/javer/codewars/eightkyu/FIXMEReplaceAllDots.java
@@ -0,0 +1,17 @@
+package javer.codewars.eightkyu;
+
+/*
+The code provided is supposed replace all the dots . in the specified String str with dashes -
+But it's not working properly.
+Task
+Fix the bug so we can all go home early.
+Notes
+String str will never be null.
+ */
+
+public class FIXMEReplaceAllDots {
+
+    public static String replaceDots(final String str) {
+        return str.replaceAll(".", "-");
+    }
+}
