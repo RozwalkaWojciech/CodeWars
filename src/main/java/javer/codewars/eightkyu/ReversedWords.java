84fd399488997280194763ad9c572c969997e8ac
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2021-06-14 21:56:15 +0200

    ReversedWords solution2

--- src/main/java/javer/codewars/eightkyu/ReversedWords.java
+++ src/main/java/javer/codewars/eightkyu/ReversedWords.java
@@ -6,11 +6,21 @@ Example:
 "The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
  */
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import static java.util.Arrays.stream;
 
 public class ReversedWords {
 
     public static String reverseWords(String str) {
         return stream(str.split(" ")).reduce(((s1, s2) -> s2 + " " + s1)).get();
+    }
+
+    public static String reverseWords2(String str) {
+        List<String> list = Arrays.asList(str.split(" "));
+        Collections.reverse(list);
+        return String.join(" ", list);
     }
 }
