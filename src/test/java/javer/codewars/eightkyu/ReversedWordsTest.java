88339a4e373015272195e09c1ff0dd9666b0ae12
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2021-06-14 21:32:51 +0200

    ReversedWords test

---
+++ src/test/java/javer/codewars/eightkyu/ReversedWordsTest.java
@@ -0,0 +1,15 @@
+package javer.codewars.eightkyu;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ReversedWordsTest {
+
+    @Test
+    public void testSomething() {
+        assertEquals("eating like I", ReversedWords.reverseWords("I like eating"));
+        assertEquals("flying like I", ReversedWords.reverseWords("I like flying"));
+        assertEquals("nice is world The", ReversedWords.reverseWords("The world is nice"));
+    }
+}
