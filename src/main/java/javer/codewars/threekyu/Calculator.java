d09d1dd212ccdddbcd8e25bcc496c96cce8e3f1f
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2021-06-16 23:16:38 +0200

    Calculate start and instruction

---
+++ src/main/java/javer/codewars/threekyu/Calculator.java
@@ -0,0 +1,17 @@
+package javer.codewars.threekyu;
+
+/*
+Create a simple calculator that given a string of operators (), +, -, *, / and numbers separated by spaces returns the value of that expression
+Example:
+Calculator.evaluate("2 / 2 + 3 * 4 - 6") // => 7
+Remember about the order of operations! Multiplications and divisions have a higher priority and should be performed left-to-right.
+Additions and subtractions have a lower priority and should also be performed left-to-right.
+ */
+
+public class Calculator {
+
+    public static Double evaluate(String expression) {
+        // your code here
+        return -1.0;
+    }
+}
