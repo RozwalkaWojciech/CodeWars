e21a201182750299cde7b7314e90649e9fc43674
wojciech.rozwalka <wojciech.rozwalka@britenet.com.pl>
2021-06-26 23:57:59 +0200

    TwiceAsOld add static import

--- src/main/java/javer/codewars/eightkyu/TwiceAsOld.java
+++ src/main/java/javer/codewars/eightkyu/TwiceAsOld.java
@@ -7,9 +7,11 @@ current age of his son (years)
 Calculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old).
  */
 
+import static java.lang.Math.abs;
+
 public class TwiceAsOld {
 
     public static int TwiceAsOld(int dadYears, int sonYears) {
-        return Math.abs(dadYears - sonYears * 2);
+        return abs(dadYears - sonYears * 2);
     }
 }
