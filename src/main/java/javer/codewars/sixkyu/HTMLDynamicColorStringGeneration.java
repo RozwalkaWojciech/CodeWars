8617d98d2cc6a07cf3fcab35c57bc063affd5082
wojciech.rozwalka <wojciech.rozwalka@britenet.com.pl>
2021-07-01 22:19:21 +0200

    HTMLDynamicColorStringGeneration start and instruction

---
+++ src/main/java/javer/codewars/sixkyu/HTMLDynamicColorStringGeneration.java
@@ -0,0 +1,26 @@
+package javer.codewars.sixkyu;
+
+/*
+Generate a valid randomly generated rgb color string. Assume all of them have 6 digits always.
+
+Valid Output
+#ffffff
+#25a403
+#000001
+
+Non-Valid Output
+#fff
+#aaa
+#zzzzz
+cafebabe
+#a567567676576756A7
+ */
+
+import java.util.Random;
+
+public class HTMLDynamicColorStringGeneration {
+
+    public static String generateColor(Random r) {
+        return "";
+    }
+}
