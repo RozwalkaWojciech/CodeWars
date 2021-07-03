419633581ada5cd287d518cad1812d8c8be532cc
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2021-07-03 20:45:54 +0200

    add BuildSquare new test

--- src/test/java/javer/codewars/sevenkyu/BuildSquareTest.java
+++ src/test/java/javer/codewars/sevenkyu/BuildSquareTest.java
@@ -5,10 +5,18 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class BuildSquareTest {
+
     @Test
     public void exampleTests() {
         assertEquals("+++\n+++\n+++", BuildSquare.generateShape(3));
         assertEquals("+++++\n+++++\n+++++\n+++++\n+++++", BuildSquare.generateShape(5));
         assertEquals("++++++++\n++++++++\n++++++++\n++++++++\n++++++++\n++++++++\n++++++++\n++++++++", BuildSquare.generateShape(8));
+    }
+
+    @Test
+    public void exampleTests2() {
+        assertEquals("+++\n+++\n+++", BuildSquare.generateShape2(3));
+        assertEquals("+++++\n+++++\n+++++\n+++++\n+++++", BuildSquare.generateShape2(5));
+        assertEquals("++++++++\n++++++++\n++++++++\n++++++++\n++++++++\n++++++++\n++++++++\n++++++++", BuildSquare.generateShape2(8));
     }
 }
