281aa04c621849758837c7003553dcd69597c3d4
wojciech.rozwalka <wojciech.rozwalka@britenet.com.pl>
2021-07-05 15:21:11 +0200

    HTMLDynamicColorStringGeneration refactor

--- src/test/java/javer/codewars/sixkyu/HTMLDynamicColorStringGenerationTest.java
+++ src/test/java/javer/codewars/sixkyu/HTMLDynamicColorStringGenerationTest.java
@@ -4,12 +4,13 @@ import org.junit.Test;
 
 import java.util.Random;
 
+import static javer.codewars.sixkyu.HTMLDynamicColorStringGeneration.generateColor;
 import static org.junit.Assert.assertEquals;
 
 public class HTMLDynamicColorStringGenerationTest {
 
     @Test
     public void symbolTest() {
-        assertEquals("#", HTMLDynamicColorStringGeneration.generateColor(new Random()).substring(0, 1));
+        assertEquals("#", generateColor(new Random()).substring(0, 1));
     }
 }
