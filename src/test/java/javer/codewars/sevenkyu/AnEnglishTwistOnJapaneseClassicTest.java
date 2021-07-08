4d22b018ed6734e5b42dc5e53b5ddf2beba85d6b
wojciech.rozwalka <wojciech.rozwalka@britenet.com.pl>
2021-07-08 08:09:57 +0200

    AnEnglishTwistOnJapaneseClassicTest add tests

--- src/test/java/javer/codewars/sevenkyu/AnEnglishTwistOnJapaneseClassicTest.java
+++ src/test/java/javer/codewars/sevenkyu/AnEnglishTwistOnJapaneseClassicTest.java
@@ -60,4 +60,46 @@ public class AnEnglishTwistOnJapaneseClassicTest {
         ArrayList<String> expected = new ArrayList<String>(Arrays.asList("ab", "bc"));
         assertEquals(expected, S.theGame(words));
     }
+
+    @Test
+    public void test13() {
+        ArrayList<String> words = new ArrayList<String>(Arrays.asList("dog", "goose", "elephant", "tiger", "rhino", "orc", "cat"));
+        List<String> expected = new ArrayList<String>(Arrays.asList("dog", "goose"
+                , "elephant", "tiger", "rhino", "orc", "cat"));
+        assertEquals(expected, S.theGame3(words));
+    }
+
+    @Test
+    public void test23() {
+        ArrayList<String> words = new ArrayList<String>(Arrays.asList("dog", "goose", "tiger", "cat", "elephant", "rhino", "orc"));
+        List<String> expected = new ArrayList<String>(Arrays.asList("dog", "goose"));
+        assertEquals(expected, S.theGame3(words));
+    }
+
+    @Test
+    public void test33() {
+        ArrayList<String> words = new ArrayList<String>(Arrays.asList("dog", "goose", "elephant", "tiger", "cat", "rhino", "rhino", "orc"));
+        List<String> expected = new ArrayList<String>(Arrays.asList("dog", "goose"
+                , "elephant", "tiger"));
+        assertEquals(expected, S.theGame3(words));
+    }
+
+    @Test
+    public void TestEmpty3() {
+        ArrayList<String> words = new ArrayList<String>();
+        assertEquals(new ArrayList<String>(), S.theGame3(words));
+    }
+
+    @Test
+    public void TestEStrings3() {
+        ArrayList<String> words = new ArrayList<String>(Arrays.asList("", "", "", "", "", "", ""));
+        assertEquals(new ArrayList<String>(), S.theGame3(words));
+    }
+
+    @Test
+    public void TestMidEStrings3() {
+        ArrayList<String> words = new ArrayList<String>(Arrays.asList("ab", "bc", "", "de", "", "", ""));
+        ArrayList<String> expected = new ArrayList<String>(Arrays.asList("ab", "bc"));
+        assertEquals(expected, S.theGame3(words));
+    }
 }
