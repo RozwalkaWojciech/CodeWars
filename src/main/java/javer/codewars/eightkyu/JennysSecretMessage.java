828501aec0bbdf93bdac5d1a1eb00d83d8426280
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2021-08-22 22:32:35 +0200

    solution JennysSecretMessage

--- src/main/java/javer/codewars/eightkyu/JennysSecretMessage.java
+++ src/main/java/javer/codewars/eightkyu/JennysSecretMessage.java
@@ -9,4 +9,10 @@ Can you help her?
 
 public class JennysSecretMessage {
 
+    public static String greet(String name) {
+        if (name.equals("Johnny")) {
+            return "Hello, my love!";
+        }
+        return String.format("Hello, %s!", name);
+    }
 }
