5aef07a22ef5b470c89bcab7d74e10a2f28f909f
wojciech.rozwalka <wojciech.rozwalka@britenet.com.pl>
2021-08-30 07:29:42 +0200

    refactor

--- src/main/java/javer/codewars/sixkyu/BreakCamelCase.java
+++ src/main/java/javer/codewars/sixkyu/BreakCamelCase.java
@@ -2,13 +2,14 @@ package javer.codewars.sixkyu;
 
 /*Complete the solution so that the function will break up camel casing, using a space between words.*/
 
+import static java.lang.Character.isUpperCase;
 import static java.lang.System.out;
 
 public class BreakCamelCase {
     public static String camelCase(String input) {
         var sb = new StringBuilder();
         for (char c : input.toCharArray()) {
-            if (Character.isUpperCase(c)) {
+            if (isUpperCase(c)) {
                 sb.append(" ").append(c);
             } else {
                 sb.append(c);
