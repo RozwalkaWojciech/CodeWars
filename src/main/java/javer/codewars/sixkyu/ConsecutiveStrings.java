d441d3e977f89e7114350b21de886c7c79f86b33
wojciech.rozwalka <wojciech.rozwalka@britenet.com.pl>
2021-09-02 07:59:09 +0200

    refactor

--- src/main/java/javer/codewars/sixkyu/ConsecutiveStrings.java
+++ src/main/java/javer/codewars/sixkyu/ConsecutiveStrings.java
@@ -64,9 +64,9 @@ public class ConsecutiveStrings {
         if (k > strarr.length || k <= 0)
             return "";
         var longestStr = "";
-        for (int index = 0; index < strarr.length - k + 1; index++) {
+        for (var index = 0; index < strarr.length - k + 1; index++) {
             var sb = new StringBuilder();
-            for (int i = index; i < index + k; i++) {
+            for (var i = index; i < index + k; i++) {
                 sb.append(strarr[i]);
             }
             if (sb.toString().length() > longestStr.length()) {
