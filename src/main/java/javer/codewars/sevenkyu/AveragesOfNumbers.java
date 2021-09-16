a4d8de3369588e990580ba424d06b28358b725d0
wojciech.rozwalka <wojciech.rozwalka@britenet.com.pl>
2021-09-16 09:38:06 +0200

    refactor

--- src/main/java/javer/codewars/sevenkyu/AveragesOfNumbers.java
+++ src/main/java/javer/codewars/sevenkyu/AveragesOfNumbers.java
@@ -4,7 +4,7 @@ public class AveragesOfNumbers {
 
     public static double[] averages(int[] numbers) {
         double[] arr = new double[numbers.length - 1];
-        for (int i = 0; i < arr.length; i++) {
+        for (var i = 0; i < arr.length; i++) {
             arr[i] = (numbers[i] + numbers[i + 1]) / 2.0;
         }
         return arr;
