e958ab425e091427a6b52e99d717de4470b3ea18
wojciech.rozwalka <wojciech.rozwalka@britenet.com.pl>
2021-10-07 16:40:27 +0200

    refactor

--- src/main/java/javer/codewars/sixkyu/WriteNumberInExpandedForm.java
+++ src/main/java/javer/codewars/sixkyu/WriteNumberInExpandedForm.java
@@ -7,7 +7,7 @@ public class WriteNumberInExpandedForm {
     public static String expandedForm(int num) {
         var result = "";
 
-        for (int i = 10; i < num; i *= 10) {
+        for (var i = 10; i < num; i *= 10) {
             int remain = num % i;
             result = (remain > 0) ? " + " + remain + result : result;
             num -= remain;
