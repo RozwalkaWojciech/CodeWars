d0c78d92e034b8c580cff18759ad1492698d71b9
wojciech.rozwalka <wojciech.rozwalka@britenet.com.pl>
2021-10-15 00:01:24 +0200

    refactor

--- src/main/java/javer/codewars/eightkyu/SumOfPositive.java
+++ src/main/java/javer/codewars/eightkyu/SumOfPositive.java
@@ -8,7 +8,7 @@ public class SumOfPositive {
 
         int result = 0;
 
-        for (int i = 0; i < arr.length; i++) {
+        for (var i = 0; i < arr.length; i++) {
             if (arr[i] > 0) {
                 result += arr[i];
             }
