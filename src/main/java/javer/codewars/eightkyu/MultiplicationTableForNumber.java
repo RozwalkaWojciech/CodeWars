c2cddc7bdc74862040236a8ca63cd95af9d6d7ed
wojciech.rozwalka <wojciech.rozwalka@britenet.com.pl>
2021-10-23 17:16:19 +0200

    refactor

--- src/main/java/javer/codewars/eightkyu/MultiplicationTableForNumber.java
+++ src/main/java/javer/codewars/eightkyu/MultiplicationTableForNumber.java
@@ -24,7 +24,7 @@ public class MultiplicationTableForNumber {
 
     public static String multiTable(int num) {
         var sb = new StringBuilder();
-        for (int i = 1; i <= 10; i++) {
+        for (var i = 1; i <= 10; i++) {
             sb.append(i).append(" * ").append(num).append(" = ").append(i * num).append("\n");
         }
         return sb.substring(0, sb.toString().length() - 1);
