a5e5b205e6f2bb1c2b259e76ae7d326bcfca6317
wojciech.rozwalka <wojciech.rozwalka@britenet.com.pl>
2021-10-24 19:11:26 +0200

    refactor

--- src/main/java/javer/codewars/fivekyu/SimpleNumberSequence.java
+++ src/main/java/javer/codewars/fivekyu/SimpleNumberSequence.java
@@ -20,7 +20,7 @@ public class SimpleNumberSequence {
     public static int missing(String s) {
         var result = 0;
         for (var i = 1; i < Math.min(8, s.length() / 2); i++) {
-            int temp = 1;
+            var temp = 1;
             int m = Integer.parseInt(s.substring(0, i));
             int hold = i;
             while (hold < s.length()) {
