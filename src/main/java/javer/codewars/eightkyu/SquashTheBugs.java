44995a98d28a43400ea126a6a7648d69b7daba1a
wojciech.rozwalka <wojciech.rozwalka@britenet.com.pl>
2021-11-15 11:11:30 +0100

    SquashTheBugs stream solution

--- src/main/java/javer/codewars/eightkyu/SquashTheBugs.java
+++ src/main/java/javer/codewars/eightkyu/SquashTheBugs.java
@@ -7,7 +7,10 @@ Output should be the length of the longest word, as a number.
 There will only be one 'longest' word.
 */
 
+import java.util.stream.Stream;
+
 public class SquashTheBugs {
+
     public static int findLongest(final String str) {
         String[] spl = str.split(" ");
         int longest = 0;
@@ -17,5 +20,12 @@ public class SquashTheBugs {
             }
         }
         return longest;
+    }
+
+    public static int findLongest2(final String str) {
+        return Stream.of(str.split(" "))
+                .mapToInt(String::length)
+                .max()
+                .orElse(0);
     }
 }
