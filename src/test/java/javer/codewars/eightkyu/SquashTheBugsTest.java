4466c4ce7fc48001a22d44ed90e348a199f793ad
wojciech.rozwalka <wojciech.rozwalka@britenet.com.pl>
2021-11-16 06:37:26 +0100

    SquashTheBugs add test to stream solution

--- src/test/java/javer/codewars/eightkyu/SquashTheBugsTest.java
+++ src/test/java/javer/codewars/eightkyu/SquashTheBugsTest.java
@@ -15,4 +15,14 @@ public class SquashTheBugsTest {
         assertEquals(6, SquashTheBugs.findLongest("Wind your body and wiggle your belly"));
         assertEquals(7, SquashTheBugs.findLongest("Lets all go on holiday"));
     }
+
+    @Test
+    public void findLongest2() {
+        // assertEquals("expected", "actual");
+        assertEquals(7, SquashTheBugs.findLongest2("The quick white fox jumped around the massive dog"));
+        assertEquals(10, SquashTheBugs.findLongest2("Take me to tinseltown with you"));
+        assertEquals(7, SquashTheBugs.findLongest2("Sausage chops"));
+        assertEquals(6, SquashTheBugs.findLongest2("Wind your body and wiggle your belly"));
+        assertEquals(7, SquashTheBugs.findLongest2("Lets all go on holiday"));
+    }
 }
