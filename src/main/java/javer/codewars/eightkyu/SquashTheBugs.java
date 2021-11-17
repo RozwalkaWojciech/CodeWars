b574254e4b537074874dd1e2f0eb656e263c144b
wojciech.rozwalka <wojciech.rozwalka@britenet.com.pl>
2021-11-17 23:32:23 +0100

    SquashTheBugs refactor

--- src/main/java/javer/codewars/eightkyu/SquashTheBugs.java
+++ src/main/java/javer/codewars/eightkyu/SquashTheBugs.java
@@ -13,7 +13,7 @@ public class SquashTheBugs {
 
     public static int findLongest(final String str) {
         String[] spl = str.split(" ");
-        int longest = 0;
+        var longest = 0;
         for (String s : spl) {
             if (s.length() > longest) {
                 longest = s.length();
