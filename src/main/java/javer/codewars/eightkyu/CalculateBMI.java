4d74517416c778c3e63e256239412b89e543059e
wojciech.rozwalka <wojciech.rozwalka@britenet.com.pl>
2021-11-26 03:58:46 +0100

    refactor

--- src/main/java/javer/codewars/eightkyu/CalculateBMI.java
+++ src/main/java/javer/codewars/eightkyu/CalculateBMI.java
@@ -32,9 +32,17 @@ public class CalculateBMI {
 
         double bmi = weight / (height * height);
 
-        if (bmi <= 18.5) return "Underweight";
-        if (bmi <= 25) return "Normal";
-        if (bmi <= 30) return "Overweight";
+        if (bmi <= 18.5) {
+            return "Underweight";
+        }
+        ;
+        if (bmi <= 25) {
+            return "Normal";
+        }
+        ;
+        if (bmi <= 30) {
+            return "Overweight";
+        }
         return "Obese";
     }
 
