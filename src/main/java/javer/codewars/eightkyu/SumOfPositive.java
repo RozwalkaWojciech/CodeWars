16e939f81d3cb81298808296d819ac9fe904b9c2
wojciech.rozwalka <wojciech.rozwalka@britenet.com.pl>
2021-11-27 22:12:34 +0100

    refactor, add instruction

--- src/main/java/javer/codewars/eightkyu/SumOfPositive.java
+++ src/main/java/javer/codewars/eightkyu/SumOfPositive.java
@@ -2,12 +2,18 @@ package javer.codewars.eightkyu;
 
 import java.util.Arrays;
 
+/*
+You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0.
+ */
+
 public class SumOfPositive {
 
     public static int sum(int[] arr) {
-
         var result = 0;
-
         for (var i = 0; i < arr.length; i++) {
             if (arr[i] > 0) {
                 result += arr[i];
@@ -17,6 +23,6 @@ public class SumOfPositive {
     }
 
     public static int sum2(int[] arr) {
-        return Arrays.stream(arr).filter(value -> value > 0).sum();
+        return Arrays.stream(arr).filter(number -> number > 0).sum();
     }
 }
