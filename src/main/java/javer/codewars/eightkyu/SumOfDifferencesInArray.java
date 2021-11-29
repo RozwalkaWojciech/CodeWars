8a222982492e4c05da17d39251dcebbb7dddac4c
wojciech.rozwalka <wojciech.rozwalka@britenet.com.pl>
2021-11-30 00:07:43 +0100

    SumOfDifferencesInArray start and instruction

---
+++ src/main/java/javer/codewars/eightkyu/SumOfDifferencesInArray.java
@@ -0,0 +1,17 @@
+package javer.codewars.eightkyu;
+
+/*
+Your task is to sum the differences between consecutive pairs in the array in descending order.
+For example:
+sumOfDifferences([2, 1, 10])
+Returns 9
+Descending order: [10, 2, 1]
+Sum: (10 - 2) + (2 - 1) = 8 + 1 = 9
+If the array is empty or the array has only one element the result should be 0 (Nothing in Haskell).
+ */
+
+public class SumOfDifferencesInArray {
+
+    public static int sumOfDifferences(int[] arr) {
+    }
+}
