24b620731a597afdf84262cd64e260a665e302a1
wojciech.rozwalka <wojciech.rozwalka@britenet.com.pl>
2021-12-08 10:27:56 +0100

    refactor

--- src/main/java/javer/codewars/eightkyu/SumMixedArray.java
+++ src/main/java/javer/codewars/eightkyu/SumMixedArray.java
@@ -1,18 +1,17 @@
 package javer.codewars.eightkyu;
 
-/*
-Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
-Return your answer as a number.
- */
-
 import java.util.List;
 
 import static java.lang.Integer.parseInt;
 
+/*
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+Assume input will be only of Integer o String type.
+Return your answer as a number.
+ */
+
 public class SumMixedArray {
-    /*
-     * Assume input will be only of Integer o String type
-     */
+
     public int sum(List<?> mixed) {
         return mixed.stream().mapToInt(o -> parseInt(o.toString())).sum();
     }
