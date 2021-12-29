dab79ed3e04aca81f49418d1594b92e947721bc1
wojciech.rozwalka <wojciech.rozwalka@britenet.com.pl>
2021-12-29 10:07:46 +0100

    PlayingWithPassphrases refactor

--- src/main/java/javer/codewars/sixkyu/PlayingWithPassphrases.java
+++ src/main/java/javer/codewars/sixkyu/PlayingWithPassphrases.java
@@ -1,13 +1,13 @@
 package javer.codewars.sixkyu;
 
 /*Everyone knows passphrases. One can choose passphrases from poems, songs,
-movies names and so on but frequently they can be guessed due to common cultural references.
+movies names and so on, but frequently they can be guessed due to common cultural references.
 You can get your passphrases stronger by different means. One is the following:
 
-choose a text in capital letters including or not digits and non alphabetic characters,
+choose a text in capital letters including or not digits and non-alphabetic characters,
 1.shift each letter by a given number but the transformed letter must be a letter (circular shift),
 2.replace each digit by its complement to 9,
-3.keep such as non alphabetic and non digit characters,
+3.keep such as non-alphabetic and non digit characters,
 4.downcase each letter in odd position, upcase each letter in even position (the first character is in position 0),
 5.reverse the whole result.
 
@@ -25,10 +25,8 @@ import static java.lang.System.out;
 public class PlayingWithPassphrases {
 
     public static String playPass(String s, int n) {
-
         var arr = s.toLowerCase().toCharArray();
         var sb = new StringBuilder();
-
         for (var i = 0; i < arr.length; i++) {
             if (isLetter(arr[i])) {
                 if (i % 2 == 0) {
