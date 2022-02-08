ef02cfb046ae851a93210b9525779b96eeb05469
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2022-02-08 19:38:45 +0100

    WriteNumberInExpandedForm clean code

--- src/main/java/javer/codewars/sixkyu/WriteNumberInExpandedForm.java
+++ src/main/java/javer/codewars/sixkyu/WriteNumberInExpandedForm.java
@@ -1,22 +1,14 @@
 package javer.codewars.sixkyu;
 
-import static java.lang.System.out;
-
 public class WriteNumberInExpandedForm {
 
     public static String expandedForm(int num) {
         var result = "";
-
         for (var i = 10; i < num; i *= 10) {
             var remain = num % i;
             result = (remain > 0) ? " + " + remain + result : result;
             num -= remain;
         }
         return num + result;
-    }
-
-    public static void main(String[] args) {
-        out.println(expandedForm(8003));
-        out.println(expandedForm(845620));
     }
 }
