c80f9d45dc4525556517ffd7f611af17b4fa2a69
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2022-03-04 08:57:19 +0100

    ReturningStrings clean code

--- src/main/java/javer/codewars/eightkyu/ReturningStrings.java
+++ src/main/java/javer/codewars/eightkyu/ReturningStrings.java
@@ -5,9 +5,11 @@ Make a function that will return a greeting statement that uses an input;
 your program should return, "Hello, <name> how are you doing today?".
  */
 
+import static java.lang.String.format;
+
 public class ReturningStrings {
 
     public static String greet(String name) {
-        return String.format("Hello, %s how are you doing today?", name);
+        return format("Hello, %s how are you doing today?", name);
     }
 }
