b6fc780fef5e5472ea0226bdb7e8fdc5ef96b0e1
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2022-03-07 19:34:37 +0100

    CalculateBMI clean code

--- src/main/java/javer/codewars/eightkyu/CalculateBMI.java
+++ src/main/java/javer/codewars/eightkyu/CalculateBMI.java
@@ -29,17 +29,13 @@ public class CalculateBMI {
     }
 
     public static String bmi3(double weight, double height) {
-
         double bmi = weight / (height * height);
-
         if (bmi <= 18.5) {
             return "Underweight";
         }
-        ;
         if (bmi <= 25) {
             return "Normal";
         }
-        ;
         if (bmi <= 30) {
             return "Overweight";
         }
