07b2a15ba6b7e9140a052139195ed5206d59dc7e
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2022-03-18 12:46:33 +0100

    ReversedStrings refactor

--- src/main/java/javer/codewars/eightkyu/ReversedStrings.java
+++ src/main/java/javer/codewars/eightkyu/ReversedStrings.java
@@ -3,6 +3,8 @@ package javer.codewars.eightkyu;
 public class ReversedStrings {
 
     public static String solution(String str) {
-        return new StringBuilder(str).reverse().toString();
+        return new StringBuilder(str)
+                .reverse()
+                .toString();
     }
 }
