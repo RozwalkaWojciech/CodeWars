2ead4e195b76de3614906369166b740358a4c9a1
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2022-03-24 10:38:31 +0100

    NextSmallerNumberWithTheSameDigits refactor

--- src/main/java/javer/codewars/fourkyu/NextSmallerNumberWithTheSameDigits.java
+++ src/main/java/javer/codewars/fourkyu/NextSmallerNumberWithTheSameDigits.java
@@ -22,10 +22,13 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.stream.Collectors;
 
+import static java.lang.String.join;
+import static java.lang.String.valueOf;
+
 public class NextSmallerNumberWithTheSameDigits {
 
     public static long nextSmaller(long n) {
-        Character[] digitArr = String.valueOf(n)
+        Character[] digitArr = valueOf(n)
                 .chars()
                 .mapToObj(c -> (char) c)
                 .toArray(Character[]::new);
@@ -47,7 +50,7 @@ public class NextSmallerNumberWithTheSameDigits {
     }
 
     public static long nextSmaller2(long n) {
-        char[] carr = String.valueOf(n).toCharArray();
+        char[] carr = valueOf(n).toCharArray();
         int len = carr.length, i;
         for (i = len - 1; i > 0; i--) {
             if (carr[i] < carr[i - 1]) break;
@@ -63,10 +66,10 @@ public class NextSmallerNumberWithTheSameDigits {
             char temp = carr[i - 1];
             carr[i - 1] = carr[min];
             carr[min] = temp;
-            String[] sarr = String.valueOf(carr).split("");
+            String[] sarr = valueOf(carr).split("");
             java.util.Arrays.sort(sarr, i, len, java.util.Collections.reverseOrder());
-            long r = Long.parseLong(String.join("", sarr));
-            return String.valueOf(r).length() == len ? r : -1;
+            long r = Long.parseLong(join("", sarr));
+            return valueOf(r).length() == len ? r : -1;
         }
     }
 }
