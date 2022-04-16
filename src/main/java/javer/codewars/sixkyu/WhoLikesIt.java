b01163a484f0a1b43e0da32f538c31d449cd22f5
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2022-04-16 19:10:14 +0200

    WhoLikesIt refactor

--- src/main/java/javer/codewars/sixkyu/WhoLikesIt.java
+++ src/main/java/javer/codewars/sixkyu/WhoLikesIt.java
@@ -9,6 +9,8 @@ likes {"Max", "John", "Mark"} // must be "Max, John and Mark like this"
 likes {"Alex", "Jacob", "Mark", "Max"} // must be "Alex, Jacob and 2 others like this"
 */
 
+import static java.lang.String.format;
+
 public class WhoLikesIt {
     public static String whoLikesIt(String... names) {
         if (names.length == 1) {
@@ -29,12 +31,13 @@ public class WhoLikesIt {
     public static String whoLikesIt2(String... nms) {
         return switch (nms.length) {
             case 0 -> "no one likes this";
-            case 1 -> String.format("%s likes this", nms[0]);
-            case 2 -> String.format("%s and %s like this", nms[0], nms[1]);
-            case 3 -> String.format("%s, %s and %s like this", nms[0], nms[1], nms[2]);
-            default -> String.format("%s, %s and %d others like this", nms[0], nms[1], nms.length - 2);
+            case 1 -> format("%s likes this", nms[0]);
+            case 2 -> format("%s and %s like this", nms[0], nms[1]);
+            case 3 -> format("%s, %s and %s like this", nms[0], nms[1], nms[2]);
+            default -> format("%s, %s and %d others like this", nms[0], nms[1], nms.length - 2);
         };
     }
+
     public static String whoLikesIt3(String... names) {
         //Do your magic here
         return names.length == 0 ? "no one likes this" : names.length == 1 ? names[0] + " likes this" : names.length == 2 ? names[0] + " and " + names[1] + " like this" : names.length == 3 ? names[0] + ", " + names[1] + " and " + names[2] + " like this" : names[0] + ", " + names[1] + " and " + (names.length - 2) + " others like this";
