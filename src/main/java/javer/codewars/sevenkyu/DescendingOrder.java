ab9cbd6ae151d8f88fcb80540dda8e2c975be98e
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2022-04-23 20:31:12 +0200

    DescendingOrder refactor

--- src/main/java/javer/codewars/sevenkyu/DescendingOrder.java
+++ src/main/java/javer/codewars/sevenkyu/DescendingOrder.java
@@ -4,13 +4,14 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 import static java.lang.Integer.parseInt;
+import static java.lang.String.valueOf;
 import static java.util.stream.Collectors.joining;
 
 public class DescendingOrder {
 
     public static int sortDesc(final int num) {
         var sb = new StringBuilder();
-        char[] chars = String.valueOf(num).toCharArray();
+        char[] chars = valueOf(num).toCharArray();
         Arrays.sort(chars);
         for (var ch : chars) {
             sb.append(ch);
@@ -19,9 +20,9 @@ public class DescendingOrder {
     }
 
     public static int sortDecsFunctional(final int num) {
-        return parseInt(String.valueOf(num)
+        return parseInt(valueOf(num)
                 .chars()
-                .mapToObj(value -> String.valueOf(Character.getNumericValue(value)))
+                .mapToObj(value -> valueOf(Character.getNumericValue(value)))
                 .sorted(Comparator.reverseOrder())
                 .collect(joining())
         );
