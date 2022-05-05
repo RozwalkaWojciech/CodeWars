3bd10abc48dff30662d54e7f472a32ca9786129e
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2022-05-05 19:50:34 +0200

    StringEndsWith refactor

--- src/main/java/javer/codewars/sevenkyu/StringEndsWith.java
+++ src/main/java/javer/codewars/sevenkyu/StringEndsWith.java
@@ -2,7 +2,6 @@ package javer.codewars.sevenkyu;
 
 /*
 Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
-
 Examples:
 strEndsWith('abc', 'bc') -- returns true
 strEndsWith('abc', 'd') -- returns false
