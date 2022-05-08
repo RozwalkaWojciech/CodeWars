bfc112ec3308fd51476ad6f14326a138a578f3ce
RozwalkaWojciech <wojciechrozwalka@gmail.com>
2022-05-08 10:40:50 +0200

    AnEnglishTwistOnJapaneseClassic refactor

--- src/main/java/javer/codewars/sevenkyu/AnEnglishTwistOnJapaneseClassic.java
+++ src/main/java/javer/codewars/sevenkyu/AnEnglishTwistOnJapaneseClassic.java
@@ -1,5 +1,22 @@
 package javer.codewars.sevenkyu;
 
+/*
+In Japan, a game called Shiritori is played.
+The rules are simple, a group of people take turns calling out a word whose beginning syllable is the same as the previous player's ending syllable.
+For example, the first person would say the word ねこ, and the second player must make a word that starts with こ, like　こむぎ.
+This repeats until a player can not think of a word fast enough or makes a word that ends in ん, because there are no words that begin with ん　in the Japanese language.
+
+English Shiritori has the same principle, with the first and last letters of words.
+That being said the lose condition is saying a word that doesn't start with the previous word's last letter or not saying a word quick enough.
+
+For example: apple -> eggs -> salmon -> nut -> turkey ...
+
+Your Task:
+You will be given a list of strings, a transcript of an English Shiritori match.
+Your task is to find out if the game ended early, and return a list that contains every valid string until the mistake. \
+If a list is empty return an empty list. If one of the elements is an empty string, that is invalid and should be handled.
+ */
+
 import java.util.ArrayList;
 import java.util.List;
 
